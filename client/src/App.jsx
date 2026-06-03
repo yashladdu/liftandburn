4541556384135fee3f8767e46ba5538a8c6e868d
Yash Laddu <leckrajladdu@gmail.com>
2026-06-03 20:06:00 +0400

    Fix double title issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -25,6 +25,7 @@ import ProgramDetailPage from './pages/ProgramDetailPage';
 export default function App() {
   return (
     <HelmetProvider>
+      <Helmet defaultTitle="LiftAndBurn" titleTemplate="%s" />
       <BrowserRouter>
         <Navbar />
         <main>
